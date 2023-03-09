40c3a69358b6f450289d95971ddc5f10bcd3ed0b
oskarbyman <oskar.byman@gmail.com>
2023-03-09 22:59:38 +0200

    testing kafkaconsumers

--- client/client.py
+++ client/client.py
@@ -1,5 +1,7 @@
 from kafka import KafkaProducer, KafkaConsumer
 import kafka
+import argparse
+import time
 import json
 import os
 
@@ -7,25 +9,53 @@ BOOTSTRAP_SERVER = os.environ.get('KAFKA_BOOTSTRAP_SERVERS')
 KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC')
 GROUP_ID = os.environ.get('GROUP_ID')
 
-print(f"Got the following bootstrap address: {BOOTSTRAP_SERVER}")
-print(f"Got the following topic: {KAFKA_TOPIC}")
+def send_entry(producer, message: dict):
 
-# create a Kafka producer
-success = False
-while not success:
     try:
-        producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVER])
-    except kafka.errors.NoBrokersAvailable:
-        continue
-    else:
-        success = True
-# publish a message to a topic
+        producer.send(KAFKA_TOPIC, value=json.dumps(message).encode('utf-8')).get(timeout=10)
+    except kafka.errors.KafkaError:
+        # Decide what to do if produce request failed...
+        print("Entry send failed, please retry")
 
-producer.send(KAFKA_TOPIC, key=b'key', value=json.dumps({'message': 'hello world'}).encode('utf-8'))
 
-# create a Kafka consumer
-consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers=[BOOTSTRAP_SERVER], group_id=GROUP_ID)
+def main(user: str):
+    print(f"Got the following bootstrap address: {BOOTSTRAP_SERVER}")
+    print(f"Got the following topic: {KAFKA_TOPIC}")
 
-# consume messages from the topic
-for message in consumer:
-    print(message)
+    # create a Kafka producer
+    producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVER])
+    # publish a message to a topic
+    keep_going = True
+    while keep_going:
+        start = input("Please enter start kilometers: ")
+        stop = input("Please enter stop kilometers: ")
+
+        entry = {
+            "user": user,
+            "start": start,
+            "stop": stop
+        }
+
+        send_entry(producer, entry)
+
+        time.sleep(2)
+        # # consume messages from the topic
+        # print("Retrieving sent messages from topic")
+        # # create a Kafka consumer
+        # consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers=[BOOTSTRAP_SERVER], group_id=GROUP_ID,
+            # auto_offset_reset='earliest',
+            # enable_auto_commit=True,
+            # value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+        # for message in consumer:
+        #     print(message.value)
+
+        _keep_going = input("Do you want to keep going (y/n)?")
+        if not _keep_going.lower() == "y":
+            keep_going = False
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--user", "-u", default="testuser", required=False)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args.user)
